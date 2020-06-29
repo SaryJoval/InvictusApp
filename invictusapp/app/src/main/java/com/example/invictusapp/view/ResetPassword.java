a6fc62638225642026418f8f6fe1ccf9dd71bea4
Sary <saraarenasv@gmail.com>
2020-06-29 18:36:32 -0400

    Se añade actividad ResetPassword

---
+++ invictusapp/app/src/main/java/com/example/invictusapp/view/ResetPassword.java
@@ -0,0 +1,14 @@
+package com.example.invictusapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.invictusapp.R;
+import android.os.Bundle;
+
+public class ResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
