699dd6fb36d8a61b6b00120131f3488784879470
Sary <saraarenasv@gmail.com>
2020-06-29 18:21:44 -0400

    Se añade menu, iconos, logo y screen splash

--- invictusapp/app/src/main/java/com/example/invictusapp/MainActivity.java
+++ invictusapp/app/src/main/java/com/example/invictusapp/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
