7dc1de22b1f8e5c75c7a53ed21c6b1daa2dc2d7c
Sary <saraarenasv@gmail.com>
2020-07-03 04:05:45 -0400

    Se agrega formulario de reembolso, se añade scroll view en reset_password y create_account Se añade estilo raised button

--- invictusapp/app/src/main/java/com/example/invictusapp/view/Storage.java
+++ invictusapp/app/src/main/java/com/example/invictusapp/view/Storage.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -20,29 +21,37 @@ import com.example.invictusapp.MainActivity;
 import com.example.invictusapp.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.lang.ref.SoftReference;
 import java.util.concurrent.Executor;
 
 public class Storage extends AppCompatActivity {
 
     private static final int CHOOSER_IMAGES = 1;
     private static final String TAG = "STORAGE";
-    private Button btnDownload;
+    //private Button btnDownload;
     private Button btnUpload;
     private ImageView imvImage;
     private StorageReference storageReference;
+    private ProgressDialog progressDialog;
+    private StorageReference documentoRef;
+    private String downloadUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage);
 
-        btnDownload = (Button) findViewById(R.id.btnDownload);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        //btnDownload = (Button) findViewById(R.id.btnDownload);
         btnUpload = (Button) findViewById(R.id.btnUpload);
         imvImage = (ImageView) findViewById(R.id.imvImage);
+        progressDialog = new ProgressDialog(this);
+
 
         imvImage.setOnClickListener(new View.OnClickListener() {
 
@@ -51,35 +60,41 @@ public class Storage extends AppCompatActivity {
                 Intent i = new Intent();
                 i.setType("image/*");
                 i.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(i,"Selecciona una imagen"),CHOOSER_IMAGES);
+                startActivityForResult(Intent.createChooser(i,"Seleccione una imagen"),CHOOSER_IMAGES);
             }
         });
 
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                StorageReference documentoRef = storageReference.child("documento.png");
+
+                progressDialog.setTitle("Procesando...");
+                progressDialog.setMessage("Enviando solicitud de reembolso");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                documentoRef = storageReference.child("documento.png");
                 imvImage.setDrawingCacheEnabled(true);
                 imvImage.buildDrawingCache();
 
                 Bitmap bitmap = imvImage.getDrawingCache();
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.PNG,100, bos);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
 
-                byte[] documentoByte = bos.toByteArray();
+                byte[] documentoByte = baos.toByteArray();
 
                 UploadTask uploadTask = documentoRef.putBytes(documentoByte);
                 uploadTask.addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.e(TAG, "Ocurrió un error en la subida");
+                        Log.e(TAG, "Ocurrió un error en la subida del archivo");
                         e.printStackTrace();
                     }
                 }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Toast.makeText(Storage.this,"Documento subido exitosamente", Toast.LENGTH_SHORT).show();
-                        String downloadUri = taskSnapshot.getUploadSessionUri().getPath();
+                        progressDialog.dismiss();
+                        Toast.makeText(Storage.this,"Solicitud enviada exitosamente", Toast.LENGTH_LONG).show();
+                        downloadUri = taskSnapshot.getUploadSessionUri().getPath();
                         Log.w(TAG, "image URL: " + downloadUri );
                     }
                 });
@@ -95,6 +110,20 @@ public class Storage extends AppCompatActivity {
             Uri imageUri = data.getData();
             if(imageUri != null){
                 imvImage.setImageURI(imageUri);
+                documentoRef = storageReference.child("Documentos").child(imageUri.getLastPathSegment());
+                /*StorageReference filepath = storageReference.child("ReembolsoDocumentos").child(imageUri.getLastPathSegment());
+                filepath.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(Storage.this, "Archivo subido exitosamente", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Ocurrió un error en la subida del archivo");
+                        e.printStackTrace();
+                    }
+                });*/
             }
         }
     }
